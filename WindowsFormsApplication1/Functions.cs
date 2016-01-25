18e7729baedc0af636a7ccc42df0fdb58b230bbf
Ethan Randall <iJunkie22@gmail.com>
2016-01-25 12:32:19 -0500

    Add Functions.cs
    Add Functions.cs for common tasks using fieldops.
    Implemented in mlaWindow.cs.

---
+++ WindowsFormsApplication1/Functions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	static class fieldops
+	{
+		static public string CapLetter(string lc_str, int cap_pos)
+		{
+			string s1, s2, s3;
+			int lc_len = lc_str.Length;
+
+			if (lc_len == 0) {
+				return ""; // return an empty string for an empty string
+			} 
+			else { // the string isnt empty
+				s1 = lc_str.Substring (0, cap_pos);
+				s2 = lc_str.Substring (cap_pos, 1).ToUpper();
+
+				if (cap_pos + 1 < lc_len) {
+					s3 = lc_str.Substring (cap_pos + 1, lc_len - (cap_pos + 1));
+				} 
+				else {
+					s3 = "";
+				}
+
+				return s1 + s2 + s3;
+			}
+				
+		}
+
+		static public bool ItalicizeTextRange(RichTextBox rtb, int i_start, int i_stop)
+		{
+			rtb.Select (i_start, i_stop); // select the target text
+			rtb.SelectionFont = new Font (rtb.Font, FontStyle.Italic); // italicize selected text
+			rtb.Select (0, 0); // deselect the text
+			return true; // return that it worked
+		}
+
+		static public bool AppendAsItalic(RichTextBox rtb2, string i_str)
+		{
+			int i_str_len = i_str.Length; // Insertion length
+			int pre_len = rtb2.Text.Length; // Insertion start position
+			rtb2.AppendText (i_str);
+			return ItalicizeTextRange (rtb2, pre_len, i_str_len); // return whether it worked
+		}
+
+		static public string GetFieldText(TextBox tb_in, string default_text)
+		{
+			if (tb_in.Text.Length == 0) {
+				return default_text;
+			} 
+			else {
+				return tb_in.Text;
+			}
+		}
+	}
+}
+
--- WindowsFormsApplication1/mlaWindow.cs
+++ WindowsFormsApplication1/mlaWindow.cs
@@ -38,20 +38,46 @@ namespace WindowsFormsApplication1
         private void generateButton_Click(object sender, EventArgs e)
         {
 			outputBox.ReadOnly = false;
-			if (!capitalizeFirstLetter ()) {
+			titleBox.Text = fieldops.CapLetter (titleBox.Text, 0);
+			cityBox.Text = fieldops.CapLetter (fieldops.GetFieldText (cityBox, "Unknown City"), 0);
+			firstAuthorBox.Text = fieldops.CapLetter (firstAuthorBox.Text, 0);
+			lastAuthorBox.Text = fieldops.CapLetter (fieldops.GetFieldText (lastAuthorBox, "Unknown"), 0);
+			publisherBox.Text = fieldops.CapLetter (fieldops.GetFieldText (publisherBox, "Unknown Pubisher"), 0);
+			stateBox.Text = fieldops.GetFieldText (stateBox, "Unknown State").ToUpper ();
+
+			if (yearBox.Text.Length == 0) {
+				MessageBox.Show("Year number cannot be blank.");
 				outputBox.Text = "Invalid Fields";
 				outputBox.ReadOnly = true;
 				return; // Stop progression
-			};
+			}
 
-            outputBox.Text = lastAuthorBox.Text + ", ";
+			if (titleBox.Text.Length == 0) {
+				MessageBox.Show("Title cannot be blank.");
+				outputBox.Text = "Invalid Fields";
+				outputBox.ReadOnly = true;
+				return; // Stop progression
+			}
+			/*
+
+			if (!capitalizeFirstLetter ()) {
+				outputBox.Text = "Invalid Fields";
+				outputBox.ReadOnly = true;
+				return; // Stop progrlastession
+			};
+			*/
+
+			outputBox.Text = lastAuthorBox.Text;
+			if (firstAuthorBox.Text.Length > 0) {
+				outputBox.AppendText (", "); // only add comma if fname is defined
+			}
             // Capitalize first letter of each name
             try
             {
                 string[] firstNames = firstAuthorBox.Text.Split();
                 for (int i = 0; i < firstNames.Length; i++)
                 {
-                    outputBox.AppendText(firstNames[i].Substring(0, 1).ToUpper() + firstNames[i].Substring(1));
+					outputBox.AppendText(fieldops.CapLetter(firstNames[i], 0));
                     if (i != firstNames.Length - 1)
                     {
                         outputBox.AppendText(" ");
@@ -67,7 +93,7 @@ namespace WindowsFormsApplication1
             }
 
             outputBox.AppendText(". ");
-            italicizeTitle();
+			fieldops.AppendAsItalic (outputBox, titleBox.Text);
             outputBox.AppendText(". " + cityBox.Text + ": " + publisherBox.Text + ", " + yearBox.Text);
             if (medium == 0)
             {
